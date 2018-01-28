eaac1f5c285207733f78c2e52527f58c2801505a
Wesley Luyten <hi@wesleyluyten.com>
2018-01-28 15:24:55 -0500

    Add isPlainObject test

--- src/isPlainObject.js
+++ src/isPlainObject.js
@@ -1,3 +1,14 @@
-export default function isPlainObject(o) {
-  return typeof o === 'object' && o.constructor === Object;
+/**
+ * @param {any} obj The object to inspect.
+ * @returns {boolean} True if the argument appears to be a plain object.
+ */
+export default function isPlainObject(obj) {
+  if (typeof obj !== 'object' || obj === null) return false;
+
+  let proto = obj;
+  while (Object.getPrototypeOf(proto) !== null) {
+    proto = Object.getPrototypeOf(proto);
+  }
+
+  return Object.getPrototypeOf(obj) === proto;
 }
---
+++ test/isPlainObject.js
@@ -0,0 +1,20 @@
+import test from 'ava';
+import * as l from '..';
+import vm from 'vm';
+
+test('returns true only if plain object', t => {
+  function Test() {
+    this.prop = 1;
+  }
+
+  const sandbox = { fromAnotherRealm: false };
+  vm.runInNewContext('fromAnotherRealm = {}', sandbox);
+
+  t.is(l.isPlainObject(sandbox.fromAnotherRealm), true);
+  t.is(l.isPlainObject(new Test()), false);
+  t.is(l.isPlainObject(new Date()), false);
+  t.is(l.isPlainObject([1, 2, 3]), false);
+  t.is(l.isPlainObject(null), false);
+  t.is(l.isPlainObject(), false);
+  t.is(l.isPlainObject({ x: 1, y: 2 }), true);
+});
