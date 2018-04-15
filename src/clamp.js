a0ed0b5bb410d38a559269855e447114e312f623
Wesley Luyten <hi@wesleyluyten.com>
2018-04-15 16:04:26 -0400

    Fix formatting

--- src/clamp.js
+++ src/clamp.js
@@ -19,8 +19,9 @@
  */
 export default function clamp(min, max, value) {
   if (min > max) {
-    throw new Error('min must not be greater than max in clamp(min, max, value)');
+    throw new Error(
+      'min must not be greater than max in clamp(min, max, value)'
+    );
   }
-  return value < min ? min :
-    value > max ? max : value;
+  return value < min ? min : value > max ? max : value;
 }
