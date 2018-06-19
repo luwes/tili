f70783a0ad7dc6d169eda600e9d0fd2443fbe6c1
Wesley Luyten <hi@wesleyluyten.com>
2018-06-19 10:12:10 -0400

    Add more clone tests

--- test/clone.js
+++ test/clone.js
@@ -18,3 +18,17 @@ test('clones strings', function(t) {
 test('clones booleans', function(t) {
   t.is(_.clone(true), true);
 });
+
+test('clones objects', function(t) {
+  t.deepEqual(_.clone({ a: { b: 1 }}), { a: { b: 1 }});
+});
+
+test('uses the clone method if exists', function(t) {
+  const obj = {
+    a: 5,
+    clone() {
+      return 1;
+    }
+  };
+  t.is(_.clone(obj), 1);
+});
