07027b4ddfe34579acb1e636a9703af74cb2c49d
Wesley Luyten <hi@wesleyluyten.com>
2018-06-25 23:28:34 -0400

    Add test coverage for defer

--- src/defer.mjs
+++ src/defer.mjs
@@ -1,17 +1,5 @@
 /* eslint no-undef:0 */
 
-var tick;
-if (typeof process === 'object' && typeof process.nextTick === 'function') {
-  tick = process.nextTick;
-} else if (typeof Promise === 'function') {
-  var resolve = Promise.resolve();
-  tick = resolve.then.bind(resolve);
-} else if (typeof setImmediate === 'function') {
-  tick = setImmediate;
-} else {
-  tick = setTimeout;
-}
-
 /**
  * Defers invoking the func until the current call stack has cleared. Any additional arguments are provided to func when it's invoked.
  *
@@ -26,5 +14,18 @@ export default function defer(func, ...args) {
   if (typeof func != 'function') {
     throw new TypeError('Expected a function');
   }
+
+  var tick;
+  if (typeof process === 'object' && typeof process.nextTick === 'function') {
+    tick = process.nextTick;
+  } else if (typeof Promise === 'function') {
+    var resolve = Promise.resolve();
+    tick = resolve.then.bind(resolve);
+  } else if (typeof setImmediate === 'function') {
+    tick = setImmediate;
+  } else {
+    tick = setTimeout;
+  }
+
   tick(() => func(...args));
 }
--- test/defer.mjs
+++ test/defer.mjs
@@ -1,15 +1,85 @@
+/* eslint-env node */
+/* eslint no-global-assign:0 */
 import test from 'ava';
 import delay from 'delay';
-import * as l from '../src';
+import { defer } from '../src';
 
-test('defer', async (t) => {
+test('defer should throw on non function', (t) => {
+  t.throws(() => defer({}, true));
+});
+
+test.serial('defer with process.nextTick', async (t) => {
   var deferred = false;
-  l.defer((bool) => {
+
+  defer((bool) => {
     deferred = bool;
   }, true);
 
-  t.falsy(deferred);
+  t.false(deferred);
 
   await delay(1);
-  t.truthy(deferred, 'deferred the function');
+  t.true(deferred, 'deferred the function');
+});
+
+test.serial('defer with Promise', async (t) => {
+  var deferred = false;
+
+  const nextTick = process.nextTick;
+  process.nextTick = null;
+
+  defer((bool) => {
+    deferred = bool;
+  }, true);
+
+  process.nextTick = nextTick;
+
+  t.false(deferred);
+
+  await delay(1);
+  t.true(deferred, 'deferred the function');
+});
+
+test.serial('defer with setImmediate', async (t) => {
+  var deferred = false;
+
+  const nextTick = process.nextTick;
+  process.nextTick = null;
+  const tempPromise = Promise;
+  Promise = null;
+
+  defer((bool) => {
+    deferred = bool;
+  }, true);
+
+  process.nextTick = nextTick;
+  Promise = tempPromise;
+
+  t.false(deferred);
+
+  await delay(1);
+  t.true(deferred, 'deferred the function');
+});
+
+test.serial('defer with setTimeout', async (t) => {
+  var deferred = false;
+
+  const nextTick = process.nextTick;
+  process.nextTick = null;
+  const tempPromise = Promise;
+  Promise = null;
+  const tempSetImmediate = setImmediate;
+  setImmediate = null;
+
+  defer((bool) => {
+    deferred = bool;
+  }, true);
+
+  process.nextTick = nextTick;
+  Promise = tempPromise;
+  setImmediate = tempSetImmediate;
+
+  t.false(deferred);
+
+  await delay(1);
+  t.true(deferred, 'deferred the function');
 });
