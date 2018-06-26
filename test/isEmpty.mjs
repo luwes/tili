eab962d361b4ade50c84df19b1bfd9ce560bf31e
Wesley Luyten <hi@wesleyluyten.com>
2018-06-26 15:52:07 -0400

    Add isEmpty code coverage
    Signed-off-by: Wesley Luyten <hi@wesleyluyten.com>

--- test/isEmpty.mjs
+++ test/isEmpty.mjs
@@ -1,20 +1,61 @@
+/* eslint no-unused-vars:0 */
 import test from 'ava';
 import * as l from '../src';
 
-// *      isEmpty([1, 2, 3]);   //=> false
-// *      isEmpty([]);          //=> true
-// *      isEmpty('');          //=> true
-// *      isEmpty(null);        //=> true
-// *      isEmpty({});          //=> true
-// *      isEmpty({length: 0}); //=> false
+test('handles arrays', function (t) {
+  t.true(l.isEmpty([]) == true);
+  t.true(l.isEmpty(['a', 'b']) == false);
+});
 
-test('works like advertised', t => {
-  t.false(l.isEmpty([1, 2, 3]));
-  t.true(l.isEmpty([]));
-  t.true(l.isEmpty(''));
-  t.true(l.isEmpty(null));
-  t.true(l.isEmpty({}));
-  t.false(l.isEmpty({ length: 0 }));
-  t.true(l.isEmpty(new Map()));
-  t.false(l.isEmpty(new RegExp()));
+test('handles objects', function (t) {
+  t.true(l.isEmpty({}) == true);
+  t.true(l.isEmpty({ a: 'b' }) == false);
+  t.true(l.isEmpty({ length: 0 }) == false);
+});
+
+test('handles strings', function (t) {
+  t.true(l.isEmpty('') == true);
+  t.true(l.isEmpty('string') == false);
+  t.true(l.isEmpty('Error') == false);
+});
+
+test('handles numbers', function (t) {
+  t.true(l.isEmpty(0) == true);
+  t.true(l.isEmpty(42) == false);
+});
+
+test('handles functions', function (t) {
+  t.true(l.isEmpty(function(){}) == true);
+  t.true(l.isEmpty(function(a,b){}) == false);
+});
+
+test('handles nulls', function (t) {
+  t.true(l.isEmpty(null) == true);
+  t.true(l.isEmpty(undefined) == true);
+  t.true(l.isEmpty() == true);
+});
+
+test('handles booleans', function (t) {
+  t.true(l.isEmpty(false) == false);
+  t.true(l.isEmpty(true) == false);
+});
+
+test('handles maps', function (t) {
+  t.true(l.isEmpty(new Map()) == true);
+  t.true(l.isEmpty(new Map([['key', 'value']])) == false);
+});
+
+test('handles sets', function (t) {
+  t.true(l.isEmpty(new Set()) == true);
+  t.true(l.isEmpty(new Set([1,2,3,4])) == false);
+});
+
+test('handles errors', function (t) {
+  t.true(l.isEmpty(new Error()) == true);
+  t.true(l.isEmpty(new Error('')) == true);
+  t.true(l.isEmpty(new Error('test')) == false);
+});
+
+test('handles unknowns', function (t) {
+  t.true(l.isEmpty(new Date()) == false);
 });
