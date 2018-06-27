c821b79637ebfe932ca1f9035420cc5d7717696b
Wesley Luyten <me@wesleyluyten.com>
2018-06-27 15:25:47 -0400

    Add memoize test coverage

--- test/memoize.mjs
+++ test/memoize.mjs
@@ -1,18 +1,34 @@
 import test from 'ava';
 import * as l from '../src';
 
-test('memoize', function(t) {
+test('memoized version of fibonacci produces identical results', function(t) {
   var fib = function(n) {
     return n < 2 ? n : fib(n - 1) + fib(n - 2);
   };
-  t.is(fib(10), 55, 'a memoized version of fibonacci produces identical results');
-  fib = l.memoize(fib); // Redefine `fib` for memoization
-  t.is(fib(10), 55, 'a memoized version of fibonacci produces identical results');
 
+  t.is(fib(10), 55);
+  fib = l.memoize(fib); // Redefine `fib` for memoization
+  t.is(fib(10), 55);
+});
+
+test('memoize checks hasOwnProperty', function(t) {
   var o = function(str) {
     return str;
   };
+
   var fastO = l.memoize(o);
-  t.is(o('toString'), 'toString', 'checks hasOwnProperty');
-  t.is(fastO('toString'), 'toString', 'checks hasOwnProperty');
+  t.is(o('toString'), 'toString');
+  t.is(fastO('toString'), 'toString');
+});
+
+test('memoize multi argument function', function(t) {
+  var sum = function(a, b, c) {
+    return a + b + c;
+  };
+
+  t.is(sum(1, 2, 3), 6);
+  sum = l.memoize(sum); // Redefine `sum` for memoization
+  t.is(sum(1, 2, 3), 6);
+  t.is(sum(1, 2, 3), 6);
+  t.not(sum(1, 7, 3), 6);
 });
